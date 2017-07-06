4ff10716503b87f446a303c85ebe450ef49e05d4
RonelTheGreat <ronelcarlo.berino@gmail.com>
2017-07-06 16:00:59 +0800

    added new feature for logged in user (login token)

--- controllers/login-controller.php
+++ controllers/login-controller.php
@@ -14,13 +14,30 @@
     }
 
     if(!empty($username && $password)){
+      // url to redirect after checking user
+      $url = '/job-hunting-web-app/views/home.php';
+
       // check if username exists in db
       $db = new Database;
-      $user = $db->query('SELECT password FROM users WHERE username = :username', array(':username' => $username));
+      $user = $db->query('SELECT password, user_id FROM users WHERE username = :username', array(':username' => $username));
+
       if($user){
         if(password_verify($password, $user[0]['password'])){
-          echo '<pre>', print_r($password), '</pre>';
-          echo 'Ready to login!';
+          // generate cookie
+          $cstrong = true;
+          $cookie = bin2hex(openssl_random_pseudo_bytes(60, $cstrong));
+          // generate a token to be saved in db
+          $token = sha1($cookie);
+          // insert token to db
+           $db->query('INSERT INTO login_tokens(token, user_id) VALUES(:token, :user_id)',
+                      array(':token'   => $token,
+                            ':user_id' => $user[0]['user_id']));
+
+          // set cookie for the logged in user
+          setcookie('JHID', $cookie, time() + (7 * 24 * 60 * 60), '/');
+          unset($_POST);
+          header('Location: ' . $url);
+          exit;
         }else{
           echo 'Username or Password is incorrect';
         }
