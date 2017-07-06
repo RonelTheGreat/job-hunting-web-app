1f904874602a29aa0c833af5f101d45b769c9a31
RonelTheGreat <ronelcarlo.berino@gmail.com>
2017-07-06 09:48:03 +0800

    Update Database.php

--- classes/Database.php
+++ classes/Database.php
@@ -1,1 +1,34 @@
 
+<?php
+
+  class Database{
+    
+    /**
+    * handles connection to db
+    */
+    protected function handler(){
+      try {
+        $handler = new PDO('mysql:host=localhost;dbname=jobhunting;charset=utf8', 'root', '' );
+        $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $handler;
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+    }
+
+    /**
+    * handles sql statements
+    *@params $query, $params=array()
+    */
+    public function query($query, $params=[]){
+      $stmt = $this->handler()->prepare($query);
+      $stmt->execute($params);
+
+      $query_str = explode(' ', $query);
+      if($query_str[0] == 'SELECT'){
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+      }
+    }
+
+  }
