a24e66d2342f82b3dbbd84efcbcfc28fc2f5c852
RonelTheGreat <ronelcarlo.berino@gmail.com>
2017-07-06 14:46:13 +0800

    login draft

--- controllers/login-controller.php
+++ controllers/login-controller.php
@@ -1,8 +1,34 @@
-<?php
+<?php require '../classes/Database.php';
 
   $username = '';
   $password = '';
-  
+
   if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
+    if(!empty($_POST['username'])){
+      $username = $_POST['username'];
+    }
+
+    if(!empty($_POST['password'])){
+      $password = $_POST['password'];
+    }
+
+    if(!empty($username && $password)){
+      // check if username exists in db
+      $db = new Database;
+      $user = $db->query('SELECT password FROM users WHERE username = :username', array(':username' => $username));
+      if($user){
+        if(password_verify($password, $user[0]['password'])){
+          echo '<pre>', print_r($password), '</pre>';
+          echo 'Ready to login!';
+        }else{
+          echo 'Username or Password is incorrect';
+        }
+      }else{
+        echo 'Username or Password is incorrect';
+      }
+
+    }else{
+      echo 'Please don\'t leave a blank field';
+    }
   }
--- views/login.php
+++ views/login.php
@@ -3,7 +3,7 @@
 <?php include './partials/header.php'; ?>
 
   <form action="<?= htmlentities($_SERVER['PHP_SELF']);?>" method="post">
-    <input type="text" name="username" placeholder="Username or Email" value="">
+    <input type="text" name="username" placeholder="Username" value="">
     <input type="password" name="password" placeholder="Password" value="">
     <button type="submit" name="login">Sign in</button>
   </form>
