a91619e768936921df98070a50b45b0c0e6fee44
RonelTheGreat <ronelcarlo.berino@gmail.com>
2017-07-10 11:55:17 +0800

    may hiranat ako

--- views/login.php
+++ views/login.php
@@ -1,9 +1,12 @@
 <?php require '../controllers/login-controller.php'; ?>
 
+<?php  include './partials/header.php'; ?>
 
-  <form action="<?= htmlentities($_SERVER['PHP_SELF']);?>" method="post">
-    <input type="text" name="username" placeholder="Username" value="">
-    <input type="password" name="password" placeholder="Password" value="">
-    <button type="submit" name="login">Sign in</button>
-    <a href="register.php">Create a New Account </a>
-  </form>
+  <div class="sign-in-form">
+    <form action="<?= htmlentities($_SERVER['PHP_SELF']);?>" method="post">
+      <input type="text" name="username" placeholder="Username" value="">
+      <input type="password" name="password" placeholder="Password" value="">
+      <button type="submit" name="login">Sign in</button>
+      <a href="register.php">Create a New Account </a>
+    </form>
+  </div>
--- views/partials/header.php
+++ views/partials/header.php
@@ -1,4 +1,3 @@
-<?php //require '../controllers/logout.php'; ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,11 +7,18 @@
   </head>
   <body>
 
-  <nav>
-    <div class="top-nav">
-      <ul>
-        <li><a href="#">Sign in</a></li>
-        <li><a href="#">Sign out</a></li>
-      </ul>
-    </div>
-  </nav>
+    <nav>
+      <div class="login-form">
+        <form action="../controllers/login.php" method="post">
+          <input type="text" name="username" placeholder="Username" value="">
+          <input type="password" name="password" placeholder="Password" value="">
+          <button class="login-btn" type="submit" name="login">Sign in</button>
+        </form>
+      </div>
+
+      <div class="top-nav">
+        <ul>
+          <!-- <li><a href="../controllers/logout.php">Sign out</a></li> -->
+        </ul>
+      </div>
+    </nav>
