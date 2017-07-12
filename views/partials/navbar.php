e44ea7818beacf97fbe2951b0ba845d1cf3d2029
RonelTheGreat <ronelcarlo.berino@gmail.com>
2017-07-12 12:54:34 +0800

    just added an icon to logout

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -3,7 +3,7 @@
     <div class="top-nav">
       <ul>
         <?php if ($user->isLoggedIn()): ?>
-        <li><a href="../controllers/logout.php">LOGOUT</a></li>
+        <li><a href="../controllers/logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i> LOGOUT</a></li>
         <?php else:  ?>
         <li>
           <a href="../views/login.php"><i class="fa fa-sign-in" aria-hidden="true"></i> LOGIN</a>
