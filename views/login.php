49ba3050b3862a71aa166defe17d15895e8121c9
RonelTheGreat <ronelcarlo.berino@gmail.com>
2017-07-12 11:24:54 +0800

    just designed the login form

--- views/login.php
+++ views/login.php
@@ -1,11 +1,12 @@
 <?php require '../controllers/login-controller.php'; ?>
 
 <?php  include './partials/header.php'; ?>
+
   <div class="login-form">
-    <h3>Sign in to your Account</h3>
+    <h3><i class="fa fa-user-o" aria-hidden="true"></i> Sign in to your Account</h3>
     <form action="<?= htmlentities($_SERVER['PHP_SELF']);?>" method="post">
-      <p><input type="text" name="username" placeholder="Username" value=""></p>
-      <p><input type="password" name="password" placeholder="Password" value=""></p>
+      <p><input class="username" type="text" name="username" placeholder="Username" value=""></p>
+      <p><input class="password" type="password" name="password" placeholder="Password" value=""></p>
       <button class="login-btn" type="submit" name="login">Sign in</button>
       <p>
         <a href="#">Forgot Password?</a>
@@ -13,3 +14,5 @@
       </p>
     </form>
   </div>
+
+<?php //include './partials/footer.php'; ?>
--- views/partials/header.php
+++ views/partials/header.php
@@ -4,6 +4,7 @@
   <head>
     <meta charset="utf-8">
     <title>Job Hunting</title>
+    <link rel="stylesheet" href="../assets/lib/font-awesome.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
   </head>
   <body>
